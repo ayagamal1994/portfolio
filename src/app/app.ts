9de290ac6e5c63fbca77b96adba352f9c5d659ba
aya <aya.gamal71994@gmail.com>
2025-06-14 04:08:20 +0200

    first-commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Hero } from './hero/hero';
+import { About } from './about/about';
+import { Skills } from './skills/skills';
+import { Portfolio } from './portfolio/portfolio';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Hero, About, Skills, Portfolio, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
